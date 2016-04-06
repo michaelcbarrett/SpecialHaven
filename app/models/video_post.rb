bda67aef73711f245503a16cee46faa591a1cfa9
Michael Barrett <michael.barrett@rutgers.edu>
2016-04-06 17:03:52 -0400

    fixed video preview on homepage

--- app/models/video_post.rb
+++ app/models/video_post.rb
@@ -1,4 +1,12 @@
 class VideoPost < ActiveRecord::Base
 	acts_as :post
 	validates :url, presence: true
+
+	def youtube_id
+		match = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i.match(self.url)
+		if match.nil?
+			return nil
+		end
+		return match[1]
+	end
 end
